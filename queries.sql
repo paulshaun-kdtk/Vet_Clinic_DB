d67261cd740ffb3d2c0141e3d8819f577f4bffd9
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-10-12 11:02:34 +0200

    animals table create, post and query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,33 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+-- first query 
+
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+-- second query
+
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+-- 3rd query
+
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+-- 4th query
+
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+
+-- 5th query
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+-- sixth query
+
+SELECT * FROM animals WHERE neutered = true;
+
+-- 7th query
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+-- 8th query
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
