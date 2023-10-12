0d5fcc2a5de30aada0d38296cc82b9e39e5e8e9c
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-10-12 16:28:16 +0200

    day two groups and transactions

--- queries.sql
+++ queries.sql
@@ -1,33 +1,87 @@
 /*Queries that provide answers to the questions from all projects.*/
 
--- first query 
+-- Day 1
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-
--- second query
-
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-
--- 3rd query
-
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-
--- 4th query
-
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-
--- 5th query
-
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-
--- sixth query
-
 SELECT * FROM animals WHERE neutered = true;
-
--- 7th query
-
 SELECT * FROM animals WHERE name != 'Gabumon';
-
--- 8th query
-
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Day 2 (transactions)
+
+        -- Species first transaction
+BEGIN TRANSACTION;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+        -- species second transaction
+
+BEGIN TRANSACTION;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+
+        -- deletion transaction
+        
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+
+
+
+                                    -- # Questions: 
+
+-- How many animals are there?
+-- How many animals have never tried to escape?
+-- What is the average weight of animals?
+-- Who escapes the most, neutered or not neutered animals?
+-- What is the minimum and maximum weight of each type of animal?
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+                                    -- #Queres:
+
+-- 1
+SELECT COUNT(*) AS total_animals
+FROM animals;
+
+-- 2
+SELECT COUNT(*) AS animals_without_escape_attempts
+FROM animals
+WHERE escape_attempts = 0;
+
+-- 3
+SELECT AVG(weight_kg) AS average_weight
+FROM animals;
+
+-- 4
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+
+-- 5
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+-- 6
+SELECT species, AVG(escape_attempts) AS average_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INTEGER,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL(8, 2)
+    weight_kg DECIMAL(8, 2),
+    species VARCHAR(255)
 );
- 
